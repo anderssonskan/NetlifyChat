3284cf8420d370dbb5ef55ffac319081c31977c4
Emma Andersson <anderssons-emma@hotmail.com>
2024-08-16 11:41:34 +0200

    Setup-done

---
+++ src/Comps/App.jsx
@@ -0,0 +1,14 @@
+import '../Style/App.css'
+import React from 'react';
+import Chat from './Chat';
+
+function App() {
+
+  return (
+    <>
+      <Chat />
+    </>
+  )
+}
+
+export default App
---
+++ src/Comps/Chat.jsx
@@ -0,0 +1,9 @@
+function Chat() {
+    return ( 
+        <>
+            <h1>hej</h1>
+        </>
+     );
+}
+
+export default Chat;
