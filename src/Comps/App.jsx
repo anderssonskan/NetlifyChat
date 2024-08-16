028d8e6394727491e1c699b9db5df57cc75a99ac
Emma Andersson <anderssons-emma@hotmail.com>
2024-08-16 16:18:45 +0200

    to-be-continued

--- src/Comps/App.jsx
+++ src/Comps/App.jsx
@@ -5,16 +5,17 @@ import Chat from './Chat';
 import Login from './Login';
 import Register from './Register';
 import Start from './Start';
+import PrivateRoute from './AppRoutes';
 
 function App() {
 
   return (
     <Router>
       <Routes>
-
         <Route path='/' element={<Start />} />
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
+        <Route path='*' element={<NoPage />} />
       </Routes>
     </Router>
   );
