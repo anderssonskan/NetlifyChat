ff7be79f1df66a0ae0b2959e80303f5bd71ae71b
Emma Andersson <anderssons-emma@hotmail.com>
2024-08-16 16:00:59 +0200

    starting-privateRoute

--- src/Comps/Login.jsx
+++ src/Comps/Login.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
     const [formData, setFormData] = useState({
         email: '',
         password: '',
     });
+
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
 
     const handleChange = (e) => {
         setFormData({
@@ -16,6 +20,32 @@ function Login() {
     const handleSubmit = async (e) => {
         e.preventDefault();
     };
+
+    try {
+        //Skicka formulärdata till backend-server
+            const response = await fetch('https://chatify-api.up.railway.app/auth/users/{userId}', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+        
+            const data = await response.json();
+    
+            if(response.ok) {
+                //JTW
+                localStorage.setItem('token', data.token);
+                
+                navigate('/chat');
+                } else {
+                setMessage(`Log in failed: ${data.error}`);
+            }
+    
+            } catch (error) {
+                setMessage('An error occurred: ' + error.message);  
+            }
+        };
 
     return ( 
         <div>
